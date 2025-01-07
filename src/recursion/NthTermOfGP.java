e00d91297f16cd83b67edabcc3a32144d6aaaafb
Nutan <purbeynutan24@gmail.com>
2025-01-07 21:29:05 +0530

    nth term of GP for long number

--- src/recursion/NthTermOfGP.java
+++ src/recursion/NthTermOfGP.java
@@ -2,18 +2,34 @@ package recursion;
 
 public class NthTermOfGP {
     public static int nthTermOfGP(int N, int A, int R) {
-        // Write you code here
 
-        if(N==1){
-            return A;
-        }
+            // Write you code here
 
-        return (R*nthTermOfGP(N-1, A, R)) % (1000000007);
+            int mod= 1000000007;
+            long ans = (A *pow(R,N-1,mod))%mod;
 
+            return (int)ans;
     }
 
+    public static long pow(int R, int N, int mod){
+            if(N==0)
+                return 1;
+
+            long temp = pow(R,N/2,mod)%mod;
+
+            if(N%2==0){
+                return (temp *temp) % mod;
+            }
+            else {return((temp * temp) % mod * R) % mod;}
+
+        }
+
+
+
     public static void main (String arg[]){
-        System.out.printf(nthTermOfGP(5,3,2)+"");
+        System.out.println(nthTermOfGP(5,3,2)+"");
+        System.out.println(nthTermOfGP(34,17,13)+"");
+
 
     }
 
