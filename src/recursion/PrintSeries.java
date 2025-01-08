541bb3bc5139c766a73c0ab0947ec90a11c4fc3c
Nutan <purbeynutan24@gmail.com>
2025-01-08 13:54:32 +0530

    print series better approch

--- src/recursion/PrintSeries.java
+++ src/recursion/PrintSeries.java
@@ -2,31 +2,23 @@ package recursion;
 import java.util.List;
 import java.util.ArrayList;
 public class PrintSeries {
-    public static int dcr(int n, int k, List<Integer> ans){
-        if(n==0||n<0){
-            ans.add(n);
-            return n;
-        }
+
+    public static void series(int n,int k, List<Integer> ans){
         ans.add(n);
-        return dcr (n-k,k,ans);
 
-    }
-    public static void incr(int v,int k, int n, List<Integer> ans){
-        if(v>n){
-
+        if(n<0){
             return;
         }
-        ans.add(v);
-        incr (v+k,k,n,ans);
+
+        series(n-k,k,ans);
+        ans.add(n);
 
     }
     public static List<Integer> printSeries(int n, int k) {
         // Write your code here
         List<Integer> ans = new ArrayList<>();
 
-        int v= dcr(n,k,ans);
-
-        incr(v+k,k,n,ans);
+        series(n,k,ans);
 
         return ans;
     }
